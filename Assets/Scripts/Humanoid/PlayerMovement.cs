6f2da811b83c7bb9e5e9bf169819d461802950a5
Juan <juan.cabaleiro@gmail.com>
2024-10-14 00:32:51 -0300

    jump and guard animation, prefab humanoid

--- Assets/Scripts/Humanoid/PlayerMovement.cs
+++ Assets/Scripts/Humanoid/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -10,8 +11,18 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
 
     private float x, y;
+
+    public Rigidbody rb;
+    public float jumpHeight = 1;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.1f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
     void Update()
     {
+        //caminar
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
@@ -21,5 +32,30 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Velx", x);
         animator.SetFloat("Vely", y);
 
+        //guardia
+        if (Input.GetKey("f"))
+        {
+            animator.SetBool("Other", false);
+            animator.Play("Guard");
+        }
+
+        //salir de guardia
+        if (x != 0 || y != 0)
+        {
+            animator.SetBool("Other", true);
+        }
+
+        //salto
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (Input.GetKey("space") && isGrounded)
+        {
+            animator.Play("Jump");
+            Invoke("Jump", 0.5f);
+            //rb.AddForce(Vector3.up * jumpHeight, ForceMode.Impulse);
+        }
+    }
+    public void Jump()
+    {
+         rb.AddForce(Vector3.up*jumpHeight, ForceMode.Impulse);
     }
 }
